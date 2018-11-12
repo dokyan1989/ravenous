42c313f72adc14927221302a62fc34c6153e1562
dokyan1989 <38043228+dokyan1989@users.noreply.github.com>
2018-11-12 16:17:50 +0700

    Updates

---
+++ precache-manifest.274a5d7f6180695afc628e8d01431b51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "9f839d09ce85f2d9931f",
+    "url": "/ravenous/static/js/runtime~main.9f839d09.js"
+  },
+  {
+    "revision": "f24b44c1b9f3e38e67c1",
+    "url": "/ravenous/static/js/main.f24b44c1.chunk.js"
+  },
+  {
+    "revision": "e1ada48e35de0e3b8be4",
+    "url": "/ravenous/static/js/1.e1ada48e.chunk.js"
+  },
+  {
+    "revision": "f24b44c1b9f3e38e67c1",
+    "url": "/ravenous/static/css/main.850c7409.chunk.css"
+  },
+  {
+    "revision": "01d9246a4f3c0c0231b4f7a91375a7f5",
+    "url": "/ravenous/index.html"
+  }
+];
